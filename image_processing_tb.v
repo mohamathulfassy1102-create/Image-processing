5cb224b4107d2860d948ac1784c8d6b6055561c6
Mohamathul Fassy <mohamathulfassy1102@gmail.com>
2025-12-09 15:49:35 +0530

    9-12-2025

---
+++ image_processing_tb.v
@@ -0,0 +1,66 @@
+`timescale 1ns/1ps 
+`define INPUTFILENAME		 "input_IP.hex" 
+`define OUTPUTFILENAME		 "output_IP.bmp"		
+
+module tb_simulation;
+
+
+reg clk, rst_n;
+wire V_sync;
+wire H_sync;
+wire [7:0] red_0;
+wire [7:0] green_0;
+wire [7:0] blue_0;
+wire [7:0] red_1;
+wire [7:0] green_1;
+wire [7:0] blue_1;
+wire enc_done;
+
+//-------------------------------------------------
+// Components
+//-------------------------------------------------
+
+image_read 
+#(.FILE(`INPUTFILENAME))
+	u_image_read
+( 
+    .clk (clk),
+    .rst_n (rst_n),
+    .V_sync (V_sync),
+    .H_sync (H_sync),
+    .red_0 (red_0),
+    .green_0 (green_0),
+    .blue_0 (blue_0),
+    .red_1 (red_1),
+    .green_1 (green_1),
+    .blue_1 (blue_1),
+	.rd_done (enc_done)
+); 
+
+image_write 
+#(.INFILE(`OUTPUTFILENAME))
+	u_image_write
+(
+	.clk(clk),
+	.rst_n(rst_n),
+	.hsync(H_sync),
+   .DATA_WRITE_R0(red_0),
+   .DATA_WRITE_G0(green_0),
+   .DATA_WRITE_B0(blue_0),
+   .DATA_WRITE_R1(red_1),
+   .DATA_WRITE_G1(green_1),
+   .DATA_WRITE_B1(blue_1),
+	.Write_Done()
+);	
+
+initial begin 
+    clk = 0;
+    forever #10 clk = ~clk;
+end
+
+initial begin
+    rst_n = 0;
+    #25 rst_n = 1;
+end
+endmodule
+
--- image_read.v
+++ image_read.v
@@ -1,7 +1,12 @@
+// Choose the operation of code by delete // in the beginning of the selected line
+`define BRIGHTNESS_OP
+//`define INVERSION_OP
+//`define THRESHOLD_OP 
+
 module image_read #(
     parameter   WIDTH = 768,
                 HEIGHT = 512,
-                FILE = "",
+                FILE = "IP_image.hex",
                 VALUE = 100,
                 START_UP_DELAY = 100,
                 H_SYNC_DELAY = 150,
@@ -12,11 +17,12 @@ module image_read #(
 ) (
     input clk,rst_n,
     output reg H_sync,
-    output V_sync
-    output [7:0] red_0,red_1,green_0,green_1,blue_0,blue_1,
+    output V_sync,
+    output reg [7:0] red_0,red_1,green_0,green_1,blue_0,blue_1,
     output rd_done
 );
-    
+    parameter DATA_WIDTH = 8,  // data width
+    parameter SIZE_OF_LENGTH = 1179648,  // image data : 1179648 bytes: 512 * 768 *3 
 // state variables
     localparam		ST_IDLE 	= 2'b00,		
 				    ST_VSYNC	= 2'b01,			// state for creating vsync 
@@ -28,12 +34,12 @@ module image_read #(
     reg rst_dly;								// delayed reset signal
     reg V_sync_ctrl, H_sync_ctrl, data_ctrl; 				// control signal for vsync counter
     reg [8:0] V_sync_cnt, H_sync_cnt;
-    reg [7:0]   total_memory [0 : sizeOfLengthReal-1];	// memory to store  8-bit data image
-// temporary memory to save image data : size will be WIDTH*HEIGHT*3
-    integer temp_bmp   [0 : WIDTH*HEIGHT*3 - 1];			
-    integer Red  [0 : WIDTH*HEIGHT - 1]; 	
-    integer Green  [0 : WIDTH*HEIGHT - 1];	
-    integer Blue  [0 : WIDTH*HEIGHT - 1];	
+    reg [7:0] total_memory [0:SIZE_OF_LENGTH-1];	// memory to store  8-bit data image
+// temporary memoryto save image data : size will be WIDTH*HEIGHT*3
+    integer temp_bmp   [0:WIDTH*HEIGHT*3-1];			
+    integer Red  [0:WIDTH*HEIGHT-1]; 	
+    integer Green  [0:WIDTH*HEIGHT-1];	
+    integer Blue  [0:WIDTH*HEIGHT-1];	
     
     integer i, j;
 
@@ -45,18 +51,20 @@ module image_read #(
     reg [18:0] data_count; // data counting for entire pixels of the image
 
     initial begin
-        $readmemh(FILE,total_memory,0,SIZE_OF_LENGTH-1)
+        $readmemh(FILE,total_memory,0,SIZE_OF_LENGTH-1);
     end
 
     always @(start) begin // For storing image data
-        for (i = 0; i<WIDTH*HEIGHT*3 ; i = i+1) begin
-            temp_bmp[i] = total_memory[i][7:0];
-        end
-        for (i = 0; i<HEIGHT ; i = i+1) begin
-            for (j = 0; j<WIDTH ; i = i+1) begin
-                Red [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+0] // save red component
-                Green [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+1] // save green component
-                Blue [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+2] // save blue component
+        if(start == 1'b1 ) begin
+            for (i = 0; i<WIDTH*HEIGHT*3 ; i = i+1) begin
+                temp_bmp[i] = total_memory[i][7:0];
+            end
+            for (i = 0; i<HEIGHT ; i = i+1) begin
+                for (j = 0; j<WIDTH ; j = j+1) begin
+                    Red [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+0]; // save red component
+                    Green [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+1]; // save green component
+                    Blue [WIDTH*i+j] = temp_bmp [WIDTH*3*(HEIGHT-i-1)+3*j+2]; // save blue component
+                end
             end
         end
     end
@@ -93,19 +101,19 @@ module image_read #(
                     nxt_state = ST_VSYNC;
                 end
                 else begin
-                    nxt_state = ST_IDLE
+                    nxt_state = ST_IDLE;
                 end
             end
             ST_VSYNC : begin
-                if(ctrl_vsync_cnt == START_UP_DELAY) begin
-                    nxt_state = ST_HSYNC
+                if(V_sync_cnt == START_UP_DELAY) begin
+                    nxt_state = ST_HSYNC;
                 end
                 else begin
                     nxt_state = ST_VSYNC;
                 end
             end
             ST_HSYNC : begin
-                if(H_sync_cnt == HSYNC_DELAY) begin
+                if(H_sync_cnt == H_SYNC_DELAY) begin
                     nxt_state = ST_DATA;
                 end
                 else begin
@@ -113,11 +121,11 @@ module image_read #(
                 end
             end
             ST_DATA : begin
-                if(start) begin
+                if(rd_done) begin
                     nxt_state = ST_IDLE;
                 end
                 else begin
-                    if(col = width - 2) begin
+                    if(col == WIDTH - 2) begin
                         nxt_state = ST_HSYNC;
                     end
                     else begin
@@ -130,14 +138,34 @@ module image_read #(
     end
 
     always @(*) begin
+        V_sync_ctrl = 0;
+        H_sync_ctrl = 0;
+        data_ctrl = 0;
+        case (curr_state)
+            ST_VSYNC : begin V_sync_ctrl = 1; end
+            ST_HSYNC : begin H_sync_ctrl = 1; end
+            ST_DATA : begin data_ctrl = 1; end 
+        endcase
+    end
+
+    always @(posedge clk , negedge rst_n) begin
         if (!rst_n) begin
-            H_sync_ctrl <= 0;
-            V_sync_ctrl <= 0;
-            data_ctrl <= 0;
+            H_sync_cnt <= 0;
+            V_sync_cnt <= 0;
         end
         else begin
-            V_sync_cnt = (V_sync_ctrl) ? V_sync_cnt + 1 : 0;
-            H_sync_cnt = (H_sync_ctrl) ? H_sync_cnt + 1 : 0;
+            if(V_sync_ctrl) begin
+			    V_sync_cnt <= V_sync_cnt + 1; // counting for vsync
+            end
+		    else begin
+			    V_sync_cnt <= 0;
+            end	
+            if(H_sync_ctrl) begin
+			    H_sync_cnt <= H_sync_cnt + 1;	// counting for hsync
+            end		
+		    else begin
+			    H_sync_cnt <= 0;
+            end
         end
     end
     
@@ -160,7 +188,7 @@ module image_read #(
     end
 
     always @(posedge clk , negedge rst_n) begin
-        if(rst_n) begin
+        if(!rst_n) begin
             data_count <= 0;
         end
         else begin
@@ -170,7 +198,7 @@ module image_read #(
         end
     end
     assign V_sync = V_sync_ctrl;
-    assign done = (data_count == 196607) ? 1'b1 : 1'b0;
+    assign rd_done = (data_count == 196607) ? 1'b1 : 1'b0;
 
     // Image processing
     always @(*) begin
@@ -240,54 +268,92 @@ module image_read #(
                 //brightness subtraction operation
                 //red 0
                 temp_r0 = Red [WIDTH*row+col] - VALUE;
-                if(temp_r0 > 255) begin
-                    red_0 = 255;
+                if(temp_r0 < 0) begin
+                    red_0 = 0;
                 end
                 else begin
                     red_0 = Red [WIDTH*row+col] - VALUE;
                 end
                 //red 1
                 temp_r1 = Red [WIDTH*row+col+1] - VALUE;
-                if(temp_r1 > 255) begin
-                    red_1 = 255;
+                if(temp_r1 < 0) begin
+                    red_1 = 0;
                 end
                 else begin
                     red_1 = Red [WIDTH*row+col+1] - VALUE;
                 end
                 //green 0
                 temp_g0 = Green [WIDTH*row+col] - VALUE;
-                if(temp_g0 > 255) begin
-                    green_0 = 255;
+                if(temp_g0 < 0 ) begin
+                    green_0 = 0;
                 end
                 else begin
                     green_0 = Green [WIDTH*row+col] - VALUE;
                 end
                 //green 1
                 temp_g1 = Green [WIDTH*row+col+1] - VALUE;
-                if(temp_g1 > 255) begin
-                    green_1 = 255;
+                if(temp_g1 < 0) begin
+                    green_1 = 0;
                 end
                 else begin
                     green_1 = Green [WIDTH*row+col+1] - VALUE;
                 end
                 //blue 0 
                 temp_b0 = Blue [WIDTH*row+col] - VALUE;
-                if(temp_b0 > 255) begin
-                    blue_0 = 255;
+                if(temp_b0 < 0) begin
+                    blue_0 = 0;
                 end
                 else begin
                     blue_0 = Blue [WIDTH*row+col] - VALUE; 
                 end
                 //blue 1 
                 temp_b1 = Blue [WIDTH*row+col+1] - VALUE;
-                if(temp_b1 > 255) begin
-                    blue_1 = 255;
+                if(temp_b1 < 0) begin
+                    blue_1 = 0;
                 end
                 else begin
                     blue_1 = Blue [WIDTH*row+col+1] - VALUE; 
                 end
             end
-                `endif
-        end
+            `endif
+
+            // INversion Operation
+            `ifdef INVERSION_OP
+                value2 = (Blue[WIDTH*row+col]+Red[WIDTH*row+col]+Green[WIDTH*row+col])/3;
+                red_0 = 255 - value2;
+                green_0 = 255 - value2;
+                blue_0 = 255 - value2;
+                value4 = (Blue[WIDTH*row+col+1]+Red[WIDTH*row+col+1]+Green[WIDTH*row+col+1])/3;
+                red_1 = 255 - value4;
+                green_1 = 255 - value4;
+                blue_1 = 255 - value4;
+            `endif
+        
+            // Threshold Operation
+            `ifdef THRESHOLD_OP
+                value = (Blue[WIDTH*row+col]+Red[WIDTH*row+col]+Green[WIDTH*row+col])/3;
+                if (value > THRESHOLD) begin
+                    red_0 = 255;
+                    green_0 = 255;
+                    blue_0 = 255;
+                end
+                else begin
+                    red_0 = 0;
+                    green_0 = 0;
+                    blue_0 = 0;
+                end
+                value1 = (Blue[WIDTH*row+col+1]+Red[WIDTH*row+col+1]+Green[WIDTH*row+col+1])/3;
+                if (value1 > THRESHOLD) begin
+                    red_1 = 255;
+                    green_1 = 255;
+                    blue_1 = 255;
+                end
+                else begin
+                    red_1 = 0;
+                    green_1 = 0;
+                    blue_1 = 0;
+                end
+            `endif 
+        end 
     end
 endmodule
